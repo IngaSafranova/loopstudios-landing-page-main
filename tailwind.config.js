fc06e0857fb20ecfea4381d83cad711eac483f4e
ingasafranova <sfrnv@yahoo.co.uk>
2024-02-07 19:51:23 +0000

    started

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html", "./build/script/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        darkGrey: "#8c8c8c",
+        veryDarkGrey: "#696969",
+      },
+      fontSize: {
+        main: "15px",
+      },
+      fontFamily: {
+        serif: ["Alata", "sans-serif"],
+        sans: ["Josefin Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
