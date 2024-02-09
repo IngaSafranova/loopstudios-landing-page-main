db4a6218f1e3cdd4523e90744e4b23ac91c97832
ingasafranova <sfrnv@yahoo.co.uk>
2024-02-09 13:57:41 +0000

    responsive nav done and hero section

--- build/script/main.js
+++ build/script/main.js
@@ -1,11 +1,28 @@
-const hamburgerBtn = document.getElementById('mobile-toggle');
-const hamburger = document.getElementById('hamburger');
-const closeIcon = document.getElementById('close');
-const nav = document.getElementById('navbar')
-console.log(closeIcon)
-const toggle = ()=>{
-    hamburger.classList.toggle('hidden');
-    closeIcon.classList.toggle('hidden');
-    nav.classList.toggle('hidden')
 
-}
+
+const hamburgerBtn = document.getElementById("mobile-toggle");
+const hamburger = document.getElementById("hamburger");
+const closeIcon = document.getElementById("close");
+const nav = document.getElementById("navbar");
+const header = document.getElementById("header");
+const body = document.getElementById("body");
+const h1 = document.getElementById("hero");
+const navLinks = document.querySelectorAll(".nav-link");
+const navLink1 = document.getElementById('nav-link1')
+console.log(navLinks);
+
+const toggle = () => {
+  nav.classList.toggle("translate-x-full");
+   h1.classList.toggle("-z-20");
+  body.classList.toggle("bg-black");
+  header.classList.toggle("bg-black");
+  header.classList.toggle("h-full");
+  hamburger.classList.toggle("hidden");
+  closeIcon.classList.toggle("hidden");
+  console.log('hi')
+};
+
+hamburgerBtn.addEventListener("click", toggle);
+navLinks.forEach((link)=>{
+  link.addEventListener('click', toggle)
+})
